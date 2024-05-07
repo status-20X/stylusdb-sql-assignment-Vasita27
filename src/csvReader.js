94cea9c0f390ce1acec87a3523b859842d635459
Vasita27 <vasita.puppala@gmail.com>
2024-05-07 11:39:44 +0530

    11th step

--- src/csvReader.js
+++ src/csvReader.js
@@ -1,15 +1,17 @@
-const fs = require('fs');
-const csv = require('csv-parser');
+const fs = require("fs");
+const csv = require("csv-parser");
+const { parse } = require("json2csv");
 
-function readCSV(filePath) {
-    const results = [];
-
+async function readCSV(filePath) {
     return new Promise((resolve, reject) => {
+        const data = [];
         fs.createReadStream(filePath)
             .pipe(csv())
-            .on('data', (data) => results.push(data))
+            .on('data', (row) => {
+                data.push(row);
+            })
             .on('end', () => {
-                resolve(results);
+                resolve(data);
             })
             .on('error', (error) => {
                 reject(error);
@@ -17,4 +19,12 @@ function readCSV(filePath) {
     });
 }
 
-module.exports = readCSV;
+async function writeCSV(filename, data) {
+    try {
+        const csvData = parse(data);
+        fs.writeFileSync(filename, csvData);
+    } catch (error) {
+        throw error;
+    }
+}
+module.exports = { readCSV, writeCSV };
